25bc0d3338e512168aa921cd3c29df46a84813c6
Lorraine Nabua <lnabua@agileassets.com>
2022-05-08 14:05:54 -0700

    Initial commit

---
+++ src/main/java/lnabua/foodtruckfinder/FoodtruckfinderApplication.java
@@ -0,0 +1,13 @@
+package lnabua.foodtruckfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtruckfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodtruckfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lnabua/foodtruckfinder/FoodtruckfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package lnabua.foodtruckfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodtruckfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
