ad8871fc0540716821aaa32daa8013008067040e
Lorraine Nabua <lnabua@agileassets.com>
2022-05-09 13:51:22 -0700

    integrated third party API

--- frontend/src/components/RestaurantDetails.js
+++ frontend/src/components/RestaurantDetails.js
@@ -2,19 +2,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link, useParams } from "react-router-dom";
 import restaurantService from "../restaurant.service";
 import { useEffect, useState } from "react";
+import Geocode from "react-geocode";
 
 const RestaurantDetails = (props) => {
+
+    const [googleAddress, setGoogleAddress] = useState('');
+
+    Geocode.setApiKey("AIzaSyBhkkcJIJjPHC5sbBVnwxqlgbgDk5e9oo8");
+    Geocode.fromLatLng(String(props.restaurant.latitude), String(props.restaurant.longitude)).then(
+        (response) => {
+          const address = response.results[0].formatted_address;
+          setGoogleAddress(address);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+
 
     return (
         <div className="container">
         <div className="card text-center">
             <div className="card-body">
-                <h6 className="card-title">{props.restaurant.name}</h6>
-                <p className="card-text">{props.restaurant.address}</p>
+                <h4 className="card-title">{props.restaurant.name}</h4>
                 <ul className="list-group list-group-flush">
+                    <li className="list-group-item">{googleAddress}</li>
+                    <h6 className="card-text">Location Description</h6>
                     <li className="list-group-item">{props.restaurant.locationDescription}</li>
+                    <h6 className="card-text">Food Items</h6>
                     <li className="list-group-item">{props.restaurant.foodItems}</li>
-                    <li className="list-group-item">{props.restaurant.schedule}</li>
+                    <li className="list-group-item"><a href={props.restaurant.schedule}>Restaurant Schedule</a></li>
                 </ul>
             </div>
         </div>
